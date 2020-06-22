fe328c168ffd659bc8862dc4f13f07e198500432
Pavel Laukhin <6886834@gmail.com>
2020-06-22 16:52:01 +0300

    Initial Commit

---
+++ Segue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Segue
+//
+//  Created by Павел on 22.06.2020.
+//  Copyright © 2020 Pavel Laukhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
